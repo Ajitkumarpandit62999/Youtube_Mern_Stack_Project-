59efc3ec1f69f4451c8b15f4bc8161b2fce920e8
Ajitkumarpandit <ajitkumarpandit647@gmail.com>
2024-10-19 14:34:20 +0530

    utils code concept

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import connectDB from "./database/connect.js";
 import dotenv from "dotenv"
+import {app} from "./app.js"
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000 , ()=>{
+        console.log(`Server is listening from port : ${process.env.PORT}`)
+    })
+
+    app.on("error in app" , (err)=>{
+        console.log("error" , err);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection  failed !!! " , err)
+})
 
